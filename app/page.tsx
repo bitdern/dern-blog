701a3c3777b42052fbd423d6441806d47d683055
bitdern <acnewell09@gmail.com>
2023-02-26 12:14:21 -0600

    seperating functions into their own component folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,11 @@
-import fs from "fs";
 import Link from "next/link";
-import matter from "gray-matter";
-import { PostMetadata } from "compontents/PostMetadata";
-
-const getPostMetaData = (): PostMetadata[] => {
-  const folder = "posts/";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
-
-  // get gray-matter data from each file
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf-8");
-    const matterResult = matter(fileContents);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      description: matterResult.data.description,
-      slug: fileName.replace(".md", ""),
-    };
-  });
-
-  return posts;
-};
+import getPostMetaData from "compontents/getPostMetadata";
+import PostPreview from "compontents/PostPreview";
 
 const HomePage = () => {
   const postMetaData = getPostMetaData();
   const postPreviews = postMetaData.map((post) => (
-    <div>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
-      <p>{post.description}</p>
-      <p>{post.date}</p>
-    </div>
+    <PostPreview key={post.slug} {...post} />
   ));
 
   return <div>{postPreviews}</div>;
