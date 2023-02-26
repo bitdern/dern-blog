125a4b1d232f5f90e9a6776568860335dcaf0593
bitdern <acnewell09@gmail.com>
2023-02-26 10:43:31 -0600

    updating some file configurations and working on blog home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+import fs from "fs";
+import Link from "next/link";
+
+const getPostMetaData = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
+
 const HomePage = () => {
-  return <p>blogs will go here</p>;
+  const postMetaData = getPostMetaData();
+  const postPreviews = postMetaData.map((slug) => (
+    <div>
+      <Link href={`/posts/${slug}`}>
+        <h2>{slug}</h2>
+      </Link>
+    </div>
+  ));
+
+  return <div>{postPreviews}</div>;
 };
 
 export default HomePage;
