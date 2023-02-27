8f2fbabed6207efbc9dd5e254d64eb82151ad3ce
bitdern <acnewell09@gmail.com>
2023-02-27 12:35:38 -0600

    messing around with styles

--- compontents/PostPreview.tsx
+++ compontents/PostPreview.tsx
@@ -6,12 +6,12 @@ const PostPreview = (props: PostMetadata) => {
   return (
     <div
       className="border border-slate-300 p-4 rounded-md shadow-sm
-    bg-white"
+      bg-white"
     >
       <p className="text-slate-400">{props.date}</p>
 
       <Link href={`/posts/${props.slug}`}>
-        <h2 className="font-bold text-violet-600 hover:underline mb-4">
+        <h2 className="text-violet-600 hover:border-dotted font-bold mb-4">
           {props.title}
         </h2>
       </Link>
